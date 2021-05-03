9002e3cd75af90b2d33b0eaeb9a7a9d7b3808e43
Felipe Almeida <felipewill2@gmail.com>
2021-05-03 18:56:27 -0300

    Create prevent_blank_open.js

---
+++ prevent_blank_open.js
@@ -0,0 +1,29 @@
+(() => {
+    console.log('---> loaded');
+    const BLANK = '_blank';
+    const _open = window.open;
+  
+    const open = (url, target) => {
+      console.log('---> is blank', target === BLANK);
+      if (target === BLANK) {
+        return;
+      }
+      console.log('---> args', arguments);
+      _open.call(arguments);
+    };
+  
+    const replaceOpen = () => {
+      console.log('---> replacing');
+      window.open = open;
+    };
+
+    if (document.readyState === 'complete' || document.readyState === 'interactive') {
+      replaceOpen();
+    } else {
+      document.addEventListener(
+        'DOMContentLoaded',
+        () => replaceOpen(),
+        false,
+      );
+    }
+})();
