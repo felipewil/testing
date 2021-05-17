8674edefc7a8f875da6187f7f1a4543cff52a36f
Felipe Almeida <felipewill2@gmail.com>
2021-05-17 11:18:10 -0300

    Update user.script.js

--- user.script.js
+++ user.script.js
@@ -4,7 +4,7 @@
 // @version      0.1
 // @description  Test script
 // @include      *
-// @exclude      http*://*\.bing\.com*
+// @exclude      http.*://.*\.bing\.com.*
 // ==/UserScript==
 
 window.alert('injected');
