dbc34b2f46d83adc7823af9ced6145d32074a9b8
Felipe Almeida <felipewill2@gmail.com>
2021-05-17 11:00:24 -0300

    Create user.script.js

---
+++ user.script.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name         Test script
+// @namespace    felipe
+// @version      0.1
+// @description  Test script
+// @include      *
+// @exclude      http*://*\.bing\.com*
+// ==/UserScript==
+
+window.alert('injected');
