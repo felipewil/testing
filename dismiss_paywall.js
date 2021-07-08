589d673763d853a0465be953410527179442fe97
Felipe Almeida <felipewill2@gmail.com>
2021-07-08 10:23:07 -0300

    Update dismiss_paywall.js

--- dismiss_paywall.js
+++ dismiss_paywall.js
@@ -12,16 +12,11 @@ if (matchDomain('elmercurio.com')) {
     });
   }
 } else if (matchDomain('estadao.com.br')) {
-  console.log('set timeout');
   setTimeout(function () {
-    console.log('fire');
     const paywall = document.querySelector('#paywall-wrapper-iframe-estadao');
     const body = document.querySelector('html');
-
-     console.log('paywall', paywall);
     
     if (!paywall) {
-      console.log('try to block');
       blockElement('#paywall-wrapper-iframe-estadao', false);
     }
 
