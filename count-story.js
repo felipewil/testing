3e1c1c295984edded805bd984bd9152657589ddd
Felipe Almeida <felipewill2@gmail.com>
2021-12-08 19:25:59 -0300

    Create count-story.js

---
+++ count-story.js
@@ -0,0 +1,162 @@
+(() => {
+  const WRAPPER_SELECTOR = 'hw-story-counter';
+  const OBSERVED_ATTRIBUTE = 'hw-story-observed';
+  const COUNTED_ATTRIBUTE = 'hw-story-counted';
+  const THRESHOLD = 0.5;
+  const QUERIES = [
+    {
+      host: ['facebook\\.com'],
+      path: '/',
+      root: '#root',
+      phone: 'article._55wo._5rgr._5gh8',
+    },
+    {
+      host: ['twitter\\.com'],
+      path: '/',
+      root: '.css-1dbjc4n',
+      phone: 'div.css-1dbjc4n.r-1igl3o0.r-qklmqi.r-1adg3ll.r-1ny4l3l',
+    },
+    {
+      host: ['instagram\\.com'],
+      path: '/',
+      phone: 'article._8Rm4L.M9sTE._1gNme.h0YNM.SgTZ1',
+    }
+  ];
+
+  const addCounter = () => {
+    const styleSheet = document.createElement('style');
+    styleSheet.innerHTML = `
+    .hw-story-counter {
+      --notification-fg-color: #333;
+      --notification-bg-color: rgb(246,246,246, 0.95);
+    }
+
+      @media (prefers-color-scheme: dark) {
+        .hw-story-counter {
+          --notification-fg-color: #ccc;
+          --notification-bg-color: #202124;
+        }
+      }
+
+      .hw-story-counter {
+        display: flex;
+        position: fixed;
+        left: 20px;
+        bottom: 36px;
+        background: var(--notification-bg-color);
+        justify-content: center;
+        align-items: center;
+        border-radius: 16px;
+        z-index: 2147483647;
+        font-family: -apple-system, BlinkMacSystemFont, sans-serif;
+        -webkit-filter: drop-shadow(0px 4px 4px rgba(0,0,0,0.5));
+        filter: drop-shadow(0px 4px 4px rgba(0,0,0,0.5));
+      }
+
+      .hw-story-counter span {
+        font-size: 16px;
+        color: var(--notification-fg-color);
+        margin: 8px 10px;
+      }
+    `;
+
+    const wrapper = document.createElement('div');
+    wrapper.classList.add(WRAPPER_SELECTOR);
+
+    const counter = document.createElement('span');
+    counter.innerText = 'Stories: 0';
+
+    wrapper.appendChild(counter);
+    document.head.appendChild(styleSheet);
+    document.body.appendChild(wrapper);
+
+    return counter;
+  };
+
+  const updateCounter = (counter, count) => {
+    counter.innerText = `Stories: ${ count }`;
+  };
+
+  const run = () => {
+    const query = QUERIES.find((q) => {
+      return q.host.find((host) => location.hostname.match(host))
+          && (!query.path || location.pathname === query.path);
+    });
+
+    if (!query) { return; }
+
+    const observeIntersection = (target) => {
+      const onIntersect = (entries, observer) => {
+        for (entry of entries) {
+          if (entry.isIntersecting) {
+            const element = entry.target;
+            element.style.background = 'red';
+
+            if (element.getAttribute(COUNTED_ATTRIBUTE) === '1') {
+              observer.disconnect();
+              return;
+            }
+
+            count += 1;
+
+            element.setAttribute(COUNTED_ATTRIBUTE, '1');
+            
+            updateCounter(counter, count);
+
+            observer.disconnect();
+          }
+        }
+      };
+      
+      const options = {
+        threshold: THRESHOLD,
+      };
+      
+      const observer = new IntersectionObserver(onIntersect, options);
+      observer.observe(target);
+    };
+
+    const handleElement = (element) => {
+      if (!element || element.getAttribute(OBSERVED_ATTRIBUTE) === '1') { return; }
+
+      const isTarget = element.matches(selector)
+  
+      if (isTarget) {
+        element.setAttribute(OBSERVED_ATTRIBUTE, '1')
+        observeIntersection(element);
+      } else {
+        const elements = Array.from(element.querySelectorAll(selector));
+  
+        elements.forEach(handleElement);
+      }
+    }
+
+    const observeMutation = () => {
+      const observer = new MutationObserver((mutationsList) => {
+        for(const mutation of mutationsList) {
+          if (mutation.type === 'childList') {
+            const nodes = Array.from(mutation.addedNodes);
+  
+            for (const node of nodes) {
+              handleElement(node);
+            }
+          }
+        }
+      });
+    
+      const root = query.root ? document.querySelector(query.root) : document;
+      observer.observe(root, { childList: true, subtree: true });
+    };
+
+    const counter = addCounter();
+    const selector = navigator.userAgent.includes('iPhone') ? query.phone : '';
+    const targets = document.querySelectorAll(selector);
+    let count = 0;
+
+    targets.forEach(handleElement);
+
+    observeMutation();
+  };
+
+  run();
+})();
