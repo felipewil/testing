f9e8de5ae130767d3f667eab139ccbc50f82267e
Felipe Almeida <felipewill2@gmail.com>
2021-12-10 10:35:42 -0300

    Update linkedin_cleanup.js

--- linkedin_cleanup.js
+++ linkedin_cleanup.js
@@ -1,75 +1,71 @@
-console.log('will inject clean up');
 (() => {
-  try {
-    console.log('--> injected');
-    console.log(chrome);
-    console.log(chrome.runtime);
-    
-    chrome.runtime.sendMessage({
-      type: 'FETCH_REQUEST_MESSAGE',
-      data: {
-        url: 'https://raw.githubusercontent.com/felipewil/testing/main/linkedin_cleanup.js',
-        responseType: 'text',
-      },
-    }, (response) => {
-      console.log('fethed', response);
-    });
-  } catch(e) {
-    console.log('error', e);
-  }
+  const run = () => {
+    const url = document.location;
+    const config = {
+      host: 'linkedin\\.com',
+      listSelector: '#feed-container',
+      itemWrapperSelector: 'li.feed-item.new-feed',
+      itemSelector: '.feed-header',
+      mustHaveWords: [
+        'like', 'likes', 'follow', 'follows', 'commented', 'love', 'loves', 'support', 'supports', 'celebrate', 'celebrates', 'reacted',
+        'gostaram', 'gostou', 'seguem', 'segue', 'comentaram', 'comentou', 'amaram', 'amou', 'parabenizou', 'parabenizaram',
+        'gostaram', 'gostou', 'seguem', 'segue', 'comentaram', 'comentou', 'amaram', 'amou', 'parabenizou', 'parabenizaram', 'apoiaram', 'apoia',
+      ],
+    };
   
-  const FEED_SELECTOR = '#feed-container';
-  const WRAPPER_SELECTOR = '.feed-item.new-feed';
-  const HEADER_SELECTOR = '.feed-header';
-
-  const headers = [ ...document.querySelectorAll(`${ FEED_SELECTOR } ${ HEADER_SELECTOR }`)];
-  const mustHaveWords = [
-    'like', 'likes', 'follow', 'follows', 'commented', 'love', 'loves',
-    'gostaram', 'gostou', 'seguem', 'segue', 'comentaram', 'comentou', 'amaram', 'amou', 'parabenizou', 'parabenizaram',
-  ];
-  const feed = document.querySelector(FEED_SELECTOR);
-
-  const verifyAndRemove = (element) => {
-    console.log('will verify', element.innerText)
-
-    const matches = mustHaveWords.some((word) => {
-      return element.innerText.match(`[\\w\\s,]+ ${ word } [\\w\\s]+$`);
-    });
-
-    if (matches) {
-      console.log('will remove', element.innerText)
-
-      const container = element.closest(WRAPPER_SELECTOR);
-      //container?.parentElement?.removeChild(container);
-      container.style.backgroundColor = 'red';
-    }
-  };
-
-  const isTargetOrFindIn = (element) => {
-    const isHeader = element.classList.contains(HEADER_SELECTOR);
-
-    if (isHeader) {
-      verifyAndRemove(element);
-    } else {
-      const elements = [ ...element.querySelectorAll(HEADER_SELECTOR) ];
-
-      elements.forEach(verifyAndRemove);
-    }
-  }
-
-  isTargetOrFindIn(feed);
-
-  const observer = new MutationObserver((mutationsList) => {
-    for(const mutation of mutationsList) {
-      if (mutation.type === 'childList') {
-        const nodes = Array.from(mutation.addedNodes);
-
-        for (const node of nodes) {
-          isTargetOrFindIn(node);
-        }
+    if (!url.href.match(config.host)) { return; }
+  
+    const mustHaveWords = config.mustHaveWords;
+    const feed = document.querySelector(config.listSelector);
+  
+    if (!feed) { return; }
+  
+    const verifyAndRemove = (element) => {
+      console.log('will verify', element.innerText)
+  
+      const matches = mustHaveWords.some((word) => {
+        return element.innerText.match(`.+ ${ word } [\\w\\s]+`);
+      });
+  
+      if (matches) {
+        console.log('will remove', element.innerText)
+  
+        const container = element.closest(config.itemWrapperSelector);
+  
+        if (!container) { return; }
+        //container?.parentElement?.removeChild(container);
+        container.style.opacity = '0.3';
+      }
+    };
+  
+    const isTargetOrFindIn = (element) => {
+      const isHeader = element.classList.contains(config.itemSelector);
+  
+      if (isHeader) {
+        verifyAndRemove(element);
+      } else {
+        const elements = Array.from(element.querySelectorAll(config.itemSelector));
+  
+        elements.forEach(verifyAndRemove);
       }
     }
-  });
+  
+    isTargetOrFindIn(feed);
+  
+    const observer = new MutationObserver((mutationsList) => {
+      for(const mutation of mutationsList) {
+        if (mutation.type === 'childList') {
+          const nodes = Array.from(mutation.addedNodes);
+  
+          for (const node of nodes) {
+            isTargetOrFindIn(node);
+          }
+        }
+      }
+    });
+  
+    observer.observe(feed, { childList: true, subtree: true });
+  };
 
-  observer.observe(feed, { childList: true, subtree: true });
-})();
+  run();
+});
