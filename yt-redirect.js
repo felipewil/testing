0670193af283cb9b603d950d0c4e6236887cca05
Felipe Almeida <felipewill2@gmail.com>
2022-02-16 08:57:48 -0300

    Update yt-redirect.js

--- yt-redirect.js
+++ yt-redirect.js
@@ -5,12 +5,41 @@
   const tryRedirect = () => {
     try {
       const url = new URL(location.href);
+
+      url.searchParams.append(NO_REDIRECT_PARAM_KEY, '1');
+      history.replaceState({}, '', url.href);
+      window.location.href = url.href;
+    } catch {}
+  };
+
+  const run = () => {
+    const threshold = 150;
+    const testImage = "http://www.google.com/images/phd/px.gif";
+    const dummyImage = new Image();
+    
+    const testLatency = (cb) => {
+      var tStart = new Date().getTime();
+    
+      dummyImage.src = testImage;
+      dummyImage.onload = function() {
+        var tEnd = new Date().getTime();
+        cb(tEnd-tStart);
+      };
+    }
+
+    try {
+      const url = new URL(location.href);
       const host = url.hostname;
       const isTarget = [ 'm.youtube.com', 'www.youtube.com' ].some((h) => host.includes(h));
-      if (isTarget && url.pathname.startsWith('/watch') && !url.searchParams.has(NO_REDIRECT_PARAM_KEY)) {
-        url.searchParams.append(NO_REDIRECT_PARAM_KEY, '1');
-        history.replaceState({}, '', url.href);
-        window.location.href = url.href;
+      const shouldRedirect = isTarget && url.pathname.startsWith('/watch') && !url.searchParams.has(NO_REDIRECT_PARAM_KEY);
+
+      if (shouldRedirect) {
+        testLatency((avg) => {
+          console.log('download time', avg);
+          if (avg <= threshold) {
+            tryRedirect();
+          }
+        });
       }
     } catch {}
   };
@@ -19,9 +48,9 @@
     const url = location.href;
     if (url !== lastUrl) {
       lastUrl = url;
-      tryRedirect();
+      run();
     }
   }).observe(document, { subtree: true, childList: true });
 
-  tryRedirect();
+  run();
 })();
