f4002f16be45937b9db5c5f0bc83f88897e4c5de
Felipe Almeida <felipewill2@gmail.com>
2022-04-01 17:07:07 -0300

    Update roam_quick_note.js

--- roam_quick_note.js
+++ roam_quick_note.js
@@ -9,13 +9,12 @@
 // ==/UserScript==
 
 (() => {
-  const callback = () => {
-    console.log('selected text 1', document.getSelection());
-    console.log('selected text 2', '' + document.getSelection());
+  let d = '';
+  document.onmouseup = () => d = document.getSelection().toString();
 
+  const callback = () => {
     const q = location.href;
-    const d = document.getSelection ? document.getSelection().toString() : '';
-    const p = document.title
+    const p = document.title;
     
     console.log('https://roamresearch.com?text=__'+encodeURIComponent(d)+'__ — via ['+encodeURIComponent(p)+']('+encodeURIComponent(q)+') [[Quotes]]#quick-capture','Roam','toolbar=no,width=700,height=350')
     open('https://roamresearch.com?text=__'+encodeURIComponent(d)+'__ — via ['+encodeURIComponent(p)+']('+encodeURIComponent(q)+') [[Quotes]]#quick-capture','Roam','toolbar=no,width=700,height=350');
@@ -24,7 +23,6 @@
   GM.registerButton({
     id: 'roam-quick-note',
     caption: 'Quick note to Roam',
-    eventType: 'onMouseDown',
     callback,
   })
 })();
