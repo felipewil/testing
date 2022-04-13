6cd34f4ff03144ef23b26366c2d0d018d8f2ff29
Felipe Almeida <felipewill2@gmail.com>
2022-04-13 11:16:15 -0300

    Create gm_darkmode.js

---
+++ gm_darkmode.js
@@ -0,0 +1,36 @@
+// ==UserScript==
+// @name         Change dark mode
+// @version      0.0.1
+// @description  Enables/disables dark mode
+// @author       Felipe
+// @match        *
+// @grant        GM.registerButton
+// @grant        GM.darkmode
+// @noframes
+// ==/UserScript==
+
+GM.registerButton({
+  id: 'theme-1',
+  caption: 'Theme 1',
+  callback: () => {
+    GM.darkmode(true, {
+      theme: {
+        background: 'white',
+        foreground: 'red',
+      },
+    });
+  },
+});
+
+GM.registerButton({
+  id: 'theme-2',
+  caption: 'Theme 2',
+  callback: () => {
+    GM.darkmode(true, {
+      theme: {
+        background: 'red',
+        foreground: 'white',
+      },
+    });
+  },
+});
