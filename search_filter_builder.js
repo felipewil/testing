5ff2aeb96b259a7678f6bc12881543631cd6b27e
Felipe Almeida <felipewill2@gmail.com>
2022-05-27 12:49:51 -0300

    Update search_filter_builder.js

--- search_filter_builder.js
+++ search_filter_builder.js
@@ -1,10 +1,10 @@
-console.log('start');
-
 const isMobile = !window.navigator.userAgent.toLocaleLowerCase().includes('macintosh');
 const GOOGLE_CONTAINER_ID = isMobile ? '#gsr' : '#rcnt';
 const SCRIPT_ID = crypto.randomUUID().slice(0, 10);
 const CONTAINER_ID = `container-${ SCRIPT_ID }`;
+const HEADER_ID = `header-${ SCRIPT_ID }`;
 const TITLE_ID = `title-${ SCRIPT_ID }`;
+const ACTIONS_ID = `actions-${ SCRIPT_ID }`;
 const WRAPPER_ID = `wrapper-${ SCRIPT_ID }`;
 const LOADER_ID = `loader-${ SCRIPT_ID }`;
 const SHOW_ALL_BUTTON_ID = 'hw-show-all-search-domains'; // Injected from Hyperweb
@@ -50,8 +50,13 @@ const containerStyle = `
     border: 0;
   }
 
-  #${ TITLE_ID } {
+  #${ HEADER_ID } {
+    display: flex;
+    flex-direction: row;
     margin: 8px 16px;
+  }
+
+  #${ TITLE_ID } {
     font-size: 16px;
     font-weight: bold;
     font-family: Google Sans,Roboto-Medium,HelveticaNeue-Medium,Helvetica Neue,sans-serif-medium,Arial,sans-serif;
@@ -382,20 +387,47 @@ const insertContainer = (container, resultsContainer, linkSelector, containerSel
   resultsContainer.insertBefore(container, resultsContainer.firstChild);
 };
 
+const buildHeader = (title, onCustomize, onClose) => {
+  const titleEl = document.createElement('span');
+  titleEl.id = TITLE_ID;
+  titleEl.innerText = title ? `${ title } - via Hyperweb` : 'via Hyperweb';
+
+  const customizeEl = document.createElement('a');
+  customizeEl.innerHTML = `
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--! Font Awesome Pro 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M490.3 40.4C512.2 62.27 512.2 97.73 490.3 119.6L460.3 149.7L362.3 51.72L392.4 21.66C414.3-.2135 449.7-.2135 471.6 21.66L490.3 40.4zM172.4 241.7L339.7 74.34L437.7 172.3L270.3 339.6C264.2 345.8 256.7 350.4 248.4 353.2L159.6 382.8C150.1 385.6 141.5 383.4 135 376.1C128.6 370.5 126.4 361 129.2 352.4L158.8 263.6C161.6 255.3 166.2 247.8 172.4 241.7V241.7zM192 63.1C209.7 63.1 224 78.33 224 95.1C224 113.7 209.7 127.1 192 127.1H96C78.33 127.1 64 142.3 64 159.1V416C64 433.7 78.33 448 96 448H352C369.7 448 384 433.7 384 416V319.1C384 302.3 398.3 287.1 416 287.1C433.7 287.1 448 302.3 448 319.1V416C448 469 405 512 352 512H96C42.98 512 0 469 0 416V159.1C0 106.1 42.98 63.1 96 63.1H192z"/></svg>
+  `;
+  customizeEl.onclick = onCustomize;
+
+  const closeEl = document.createElement('a');
+  closeEl.innerHTML = `
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><!--! Font Awesome Pro 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M310.6 361.4c12.5 12.5 12.5 32.75 0 45.25C304.4 412.9 296.2 416 288 416s-16.38-3.125-22.62-9.375L160 301.3L54.63 406.6C48.38 412.9 40.19 416 32 416S15.63 412.9 9.375 406.6c-12.5-12.5-12.5-32.75 0-45.25l105.4-105.4L9.375 150.6c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0L160 210.8l105.4-105.4c12.5-12.5 32.75-12.5 45.25 0s12.5 32.75 0 45.25l-105.4 105.4L310.6 361.4z"/></svg>
+  `;
+  customizeEl.onclick = onClose;
+
+  const actionsEl = document.createElement('div');
+  actionsEl.id = ACTIONS_ID;
+  actionsEl.appendChild(customizeEl);
+  actionsEl.appendChild(closeEl);
+
+  const headerEl = document.createElement('div');
+  headerEl.id = HEADER_ID;
+  headerEl.appendChild(titleEl);
+  headerEl.appendChild(actionsEl);
+
+  return headerEl;
+};
+
 const run = async ({
   title,
   domains,
-  callback,
+  onCustomize,
+  onClose,
 }) => {
   const {
     query,
     linkSelector,
     containerSelector,
   } = getPageDetails();
-
-  console.log(query,
-    linkSelector,
-    containerSelector);
 
   if (!query) { return; }
 
@@ -428,10 +460,6 @@ const run = async ({
   loaderEl.id = LOADER_ID;
   loaderEl.style.opacity = '1';
 
-  const titleEl = document.createElement('span');
-  titleEl.id = TITLE_ID;
-  titleEl.innerText = title ? `${ title } - via Hyperweb` : 'via Hyperweb';
-
   const iframeWrapper = document.createElement('div');
   iframeWrapper.id = WRAPPER_ID;
 
@@ -458,18 +486,11 @@ const run = async ({
   
   iframeWrapper.appendChild(loaderEl);
   iframeWrapper.appendChild(iframeEl);
-  
-  containerEl.appendChild(titleEl);
+
+  containerEl.appendChild(buildHeader(title, onCustomize, onClose));
   containerEl.appendChild(iframeWrapper);
 
-  // sibling.parentElement.insertBefore(container, sibling);
-  console.log('insert', title, new Date());
   insertContainer(containerEl, googleContainer, linkSelector, containerSelector);
-  callback?.();
 };
 
-console.log('start 1');
 window.HyperwebSearchFilter = run;
-console.log(window);
-console.log(window.HyperwebSearchFilter);
-console.log('end');
